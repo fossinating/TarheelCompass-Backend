c5b5b9714487b36c2a290249e89dfe2ee240635c
David Foss <david@fosshome.com>
2024-05-29 06:48:53 -0400

    Add proper CORS (I think)

--- server/app.py
+++ server/app.py
@@ -1,11 +1,30 @@
+import os
 from fastapi import FastAPI
 
 from common.database import init_db
 from schema import schema
 from strawberry.fastapi import GraphQLRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-app.debug = True
+app.debug = "dev" in os.environ
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "http://localhost:80",
+] if app.debug else [
+    "https://tarheelcompass.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 graphql_app = GraphQLRouter(schema, graphiql=False)
 
