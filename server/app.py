b31d09ef11d09e1aca401c9258be873b8650c808
David Foss <david@fosshome.com>
2024-05-31 06:06:24 -0400

    pull terms directly from database, automatically selecting any that have been seen in the last week.

--- server/app.py
+++ server/app.py
@@ -1,13 +1,19 @@
 import os
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import Depends, FastAPI
+import datetime
 
-from common.database import init_db
+from common.database import init_db, session_factory
+from common.models import TermData, TermDataSource
 from schema import schema
 from strawberry.fastapi import GraphQLRouter
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import select
 
-app = FastAPI()
-app.debug = "dev" in os.environ
+dev_mode = "dev" in os.environ
+
+app = FastAPI(openapi_url="/openapi.json" if dev_mode else None, docs_url="/docs" if dev_mode else None, redoc_url=None)
+app.debug = dev_mode
 
 origins = [
     "http://localhost",
@@ -30,11 +36,15 @@ graphql_app = GraphQLRouter(schema, graphiql=False)
 
 app.include_router(graphql_app, prefix="/graphql")
 
+db_session = session_factory()
+
 
 @app.get("/terms")
 async def terms():
-    return ["FALL2023"]
+    stmt = select(TermData).where(TermData.sources.any_(TermDataSource.last_seen < (datetime.datetime.now() + datetime.timedelta(days=7))))
+    result = db_session.execute(stmt)
 
+    return [{"name": term.name, "id": term.id} for term in result.scalars()]
 
 if __name__ == '__main__':
     init_db()
