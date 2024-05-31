9bc80bdfc265b4793f18f48bfe47dfd59da414e4
David Foss <david@fosshome.com>
2024-05-31 07:11:11 -0400

    update staging environment

--- staging_environment/stage.sh
+++ staging_environment/stage.sh
@@ -4,11 +4,20 @@ sudo rm data/postgres -r
 docker compose rm --all --stop --force
 
 source .env
-echo $CR_PAT | docker login ghcr.io -u fossinating --password-stdin
 
-docker image pull ghcr.io/fossinating/tarheel-compass-data:staging
-docker image pull ghcr.io/fossinating/tarheel-compass-server:staging
+if ! echo $CR_PAT | docker login ghcr.io -u fossinating --password-stdin; then
+echo "Failed to log in, stopping"
+exit 1
+fi
 
-docker compose up -d
+if ! docker image pull ghcr.io/fossinating/tarheel-compass-data:latest && docker image pull ghcr.io/fossinating/tarheel-compass-server:latest; then
+echo "Failed to pull images"
+exit 1
+fi
+
+if ! docker compose up -d; then
+echo "Failed to start"
+exit 1
+fi
 
 # TODO: Stage with a copy of the live database
